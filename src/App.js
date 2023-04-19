b6b053668e20684f1bec0ef88da5777ed2bda985
yalmeida <y.almeida0307@gmail.com>
2023-04-18 20:01:17 -0700

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import axios from 'axios';
 
-function App() {
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      playerName: null,
+      playerStats: {}
+    }
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.getPlayerId()
+    console.log(this.state.playerName)
+  }
+
+  handleChange = (event) => {
+    const replace = event.target.value.split(" ").join("_");
+    if(replace.length > 0){
+      this.setState({playerName: replace})
+    } else {
+      alert("Please type players name")
+    }
+  }
+
+
+getPlayerId = () => {
+  axios.get(`https://www.balldontlie.io/api/v1/players?search=${this.state.playerName}`)
+  .then(async res => {
+    //console.log(res.data.data)
+    if(res.data.data[0] === undefined){
+      alert("Cant search player")
+    } else if(res.data.data.length > 1){
+      alert("Make it more specific")
+    } else{
+      await this.getPlayerStats(res.data.data[0].id)
+    }
+
+  }).catch(err => {
+    console.log(err)
+  })
+}
+
+getPlayerStats = (playerId) => {
+  axios.get(`https://www.balldontlie.io/api/v1/season_averages?season=2022&player_ids[]=${playerId}`)
+  .then(async res => {
+    console.log(res.data.data)
+    this.setState({playerStats: res.data.data[0]})
+  }).catch(err => {
+    console.log(err)
+  })
+}
+
+componentDidMount(){
+  this.getPlayerId()
+  this.getPlayerStats()
+}
+
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={this.handleSubmit}> 
+        <label>
+          Name 
+          <input
+           type="text"
+           value={this.state.value}
+           onChange={this.handleChange}
+           placeholder="please enter players name"
+          />
+          <input type="submit" value="Submit"/>
+        </label>
+      </form>
+
+      Games Played: {this.state.playerStats["games_played"]}
+      <br />
+      Points Averaged: {this.state.playerStats["pts"]}
+      <br />
+      Rebounds Averaged: {this.state.playerStats["reb"]}
+      <br />
+      Assists Averaged: {this.state.playerStats["ast"]}
+      <br />
     </div>
   );
 }
-
+}
 export default App;
